3aec811815bc6f4362220ef1ef2961c18f84004b
alexahdp <alexahdp@gmail.com>
2022-04-10 01:40:59 +0400

    Added prefix operators

--- src/parser/parser.go
+++ src/parser/parser.go
@@ -5,6 +5,7 @@ import (
 	"monkeylang/src/ast"
 	"monkeylang/src/lexer"
 	"monkeylang/src/token"
+	"strconv"
 )
 
 const (
@@ -32,6 +33,9 @@ func New(l *lexer.Lexer) *Parser {
 	
 	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
 	p.registerPrefix(token.IDENT, p.parseIdentifier)
+	p.registerPrefix(token.INT, p.parseIntegerLiteral)
+	p.registerPrefix(token.BANG, p.parsePrefixExpression)
+	p.registerPrefix(token.MINUS, p.parsePrefixExpression)
 
 	p.nextToken()
 	p.nextToken()
@@ -159,16 +163,60 @@ func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement{
 	return stmt
 }
 
-func (p *Parser) parseExpression(precedence int) ast.Expression {
-	prefix := p.prefixParseFns[p.curToken.Type]
-	if prefix == nil {
+
+
+func (p *Parser) parseIdentifier() ast.Expression {
+	return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+}
+
+func (p *Parser) parseIntegerLiteral() ast.Expression {
+	lit := &ast.IntegerLiteral{Token: p.curToken}
+
+	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+
+	if err != nil {
+		msg := fmt.Sprintf("could not parse v as integer: %s", p.curToken.Literal)
+		p.errors = append(p.errors, msg)
 		return nil
 	}
 
+	lit.Value = value
+	return lit
+}
+
+func (p *Parser) noPrefixParseFnError(t token.TokenType) {
+	msg := fmt.Sprintf("no prefix parse function for %s found", t)
+	p.errors = append(p.errors, msg)
+}
+
+// func (p *Parser) parseExpression(precedence int) ast.Expression {
+// 	prefix := p.prefixParseFns[p.curToken.Type]
+// 	if prefix == nil {
+// 		return nil
+// 	}
+
+// 	leftExp := prefix()
+// 	return leftExp
+// }
+
+func (p *Parser) parseExpression(precedence int) ast.Expression {
+	prefix := p.prefixParseFns[p.curToken.Type]
+	if prefix == nil {
+		p.noPrefixParseFnError(p.curToken.Type)
+		return nil
+	}
+	
 	leftExp := prefix()
 	return leftExp
 }
 
-func (p *Parser) parseIdentifier() ast.Expression {
-	return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+func (p *Parser) parsePrefixExpression() ast.Expression {
+	expression := &ast.PrefixExpression{
+		Token:    p.curToken,
+		Operator: p.curToken.Literal,
+	}
+	
+	p.nextToken()
+	expression.Right = p.parseExpression(PREFIX)
+	return expression
 }
